127f7258be9c122102d077ccdcc7c05c00cc185d
Chinmay12-dot <chinmaykapre06@gmail.com>
2025-04-10 22:26:27 +0530

    Delete class Employee implements Serializable{.java

--- class Employee implements Serializable{.java
+++
@@ -1,9 +0,0 @@
-class Employee implements Serializable{
-    int id;
-    String name;
-    static String company = "SSS IT Pvt Ltd";
-    public Student(int id,String name){
-        this.id = id;
-        this.name = name;
-    }
-}
